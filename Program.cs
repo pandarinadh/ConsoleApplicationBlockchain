dcbecb16e8c86a8c5c4a74c8bffd176f6573f43c
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2018-08-27 16:20:37 -0400

    getting public key and hash

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NBitcoin;
+using QBitNinja.Client;
 
 namespace ConsoleApplicationBlockChain
 {
@@ -10,6 +12,23 @@ namespace ConsoleApplicationBlockChain
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Hello World! " + new Key().GetWif(Network.Main));
+
+            Key privateKey = new Key();
+            PubKey publicKey = privateKey.PubKey;
+            Console.WriteLine(publicKey);
+             // Console.WriteLine(publicKey.GetAddress(Network.Main)); // 1PUYsjwfNmX64wS368ZR5FMouTtUmvtmTY
+            //Console.WriteLine(publicKey.GetAddress(Network.TestNet)); // n3zWAo2eBnxLr3ueohXnuAa8mTVBhxmPhq
+
+            var publicKeyHash = publicKey.Hash;
+
+            Console.WriteLine(publicKeyHash);
+
+            QBitNinjaClient client = new QBitNinjaClient(Network.TestNet);
+
+            //   var mainNetAddress = publicKeyHash.GetAddress(Network.Main);
+
+            Console.ReadLine();
         }
     }
 }
